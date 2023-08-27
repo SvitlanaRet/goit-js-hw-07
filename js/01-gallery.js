30dcf1a6706705cc40b262f33fe26345b3700e34
SvitlanaRet <s.retunska@gmail.com>
2023-08-27 22:20:48 +0300

    first task is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listElement = document.querySelector(".gallery")
 
-console.log(galleryItems);
+const renderList = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("");
+
+const handleListClick = (event) => {
+    event.preventDefault()
+    if (event.currentTarget === event.target) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}" width="1400" height="900"/>
+    </div>
+`)
+   
+    instance.show()
+
+    window.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+        instance.close()}
+    })
+}
+
+listElement.insertAdjacentHTML("beforeend", renderList)
+listElement.addEventListener('click', handleListClick)
+
+// console.log(galleryItems);
+
