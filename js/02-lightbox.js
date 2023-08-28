a4a27e4a6dfdf0987480523b6bfbfa0a36699b36
SvitlanaRet <s.retunska@gmail.com>
2023-08-28 14:42:56 +0300

    Second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listElement = document.querySelector(".gallery")
 
-console.log(galleryItems);
+const renderList = galleryItems.map(({ preview, original, description }) =>
+`<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join("")
+
+const handleListClick = (event) => {
+    event.preventDefault()
+    if (event.currentTarget === event.target) {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a',  {captionsData: 'alt',
+    captionDelay: 250}
+    );
+}
+
+
+listElement.insertAdjacentHTML("beforeend", renderList)
+listElement.addEventListener("click", handleListClick)
+
+// console.log(galleryItems);
